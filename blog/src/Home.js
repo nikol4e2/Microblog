b8f986451d59e818f30bca66e7202be9ac3b883c
nikol4e2 <arsovnikola1@gmail.com>
2024-11-29 11:25:11 +0100

    learned about useState

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,10 +1,21 @@
+import {useState} from 'react'
+
+
+
 const Home = () =>{
 
+    const [name, setName] = useState('Nikola')
+    const [age, setAge] = useState(25);
+
+    const handleClick=()=>{
+        setName("Arsov");
+        setAge(30);
+    }
     
     return(
         <div className="home">
             <h2>Homepage</h2>
-           
+           <button onClick={handleClick}> Click me</button>
                  
           
         </div>
